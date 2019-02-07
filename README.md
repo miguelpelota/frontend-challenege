5164f91417dfb5df390de36c03ac7cbbbcb6a370
Michael Ball <michael.ball@demandmedia.com>
2019-02-06 17:18:22 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Leaf Group
+
+## Media Team - Frontend Coding Challenge
+
+### Task
+
+Create a nested dropdown menu in Vue.js.
+
+### Requirements
+
+* The top level categories should be the only ones showing by default.
+* When a top level category is hovered over and it has children then the direct children should be displayed.
+* If the direct children also have children and are hovered then their direct children should be displayed.
+* Repeat the previous bullet point for as long as there are children of categories.
+* No designs are provided, so use your imagination when adding styling.
+
+### Background
+
+* The Vue CLI was used to create a basic Vue.js application.
+* Example category data is provided in `/src/App.vue` inside `data`.
+* Use this category data to created a nested dropdown menu.
+* You can start the server by running `npm run serve`.
+* Please zip up this folder and send over when completed or when time expires.
+
+# Good Luck!
---
+++ src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<!-- /////////////////////////////////////////////////////////////////////// -->
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      categories: [
+        {
+          name: 'Tours',
+          slug: 'tours',
+          children: [
+            {
+              name: 'Restaurants',
+              slug: 'restaurants',
+            },
+            {
+              name: 'Houses',
+              slug: 'houses',
+            },
+            {
+              name: 'Hotels',
+              slug: 'hotels',
+            },
+            {
+              name: 'Apartments',
+              slug: 'apartments',
+            },
+          ],
+        },
+        {
+          name: 'Design',
+          slug: 'design',
+        },
+        {
+          name: 'Landscaping',
+          slug: 'landscaping',
+          children: [
+            {
+              name: 'Plants',
+              slug: 'plants',
+              children: [
+                {
+                  name: 'Trees',
+                  slug: 'trees',
+                },
+                {
+                  name: 'Succulents',
+                  slug: 'succulents',
+                },
+                {
+                  name: 'Grass',
+                  slug: 'grass',
+                },
+              ],
+            },
+            {
+              name: 'Flower Beds & Specialty Gardens',
+              slug: 'flower-beds-specialty-gardens',
+            },
+          ],
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<!-- /////////////////////////////////////////////////////////////////////// -->
+
+<style>
+</style>
