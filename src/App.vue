9efad3e4065cba5e304c5a7e2aac0768065c17f3
Albert Park <albe.park@gmail.com>
2019-02-06 18:17:59 -0800

    solution to provided challenge

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,39 @@
 <template>
   <div id="app">
+    <h1>Welcome to the Leaf Group Store!</h1>
+    <h3>Please hover over our categories to see what is available.</h3>
+    <ol>
+      <li
+        class="main-category-text" 
+        v-for="category in categories" 
+        :key="category.name"
+        @mouseover="shownChildren = category.name" 
+        @mouseout="shownChildren = ''"
+      >
+        {{ category.name }}
+        <ol>
+          <li 
+            v-for="child in category.children" 
+            :key="child.name" 
+            v-show="shownChildren === category.name"
+            @mouseover="shownSecondChildren = child.name" 
+            @mouseout="shownSecondChildren = ''"
+          >
+            {{ child.name }}
+            <ol>
+              <li 
+                class="hover-color-blue second-child-text"
+                v-for="secondChild in child.children" 
+                :key="secondChild.name"
+                v-show="shownSecondChildren === child.name"
+              >
+                {{ secondChild.name }}
+              </li>
+            </ol>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -10,6 +44,8 @@ export default {
   name: 'App',
   data() {
     return {
+      shownChildren: '',
+      shownSecondChildren: '',
       categories: [
         {
           name: 'Tours',
@@ -74,4 +110,41 @@ export default {
 <!-- /////////////////////////////////////////////////////////////////////// -->
 
 <style>
+  #app {
+    margin: 0 auto;
+    text-align: center;
+    width: 50%;
+  }
+
+  h1 {
+    background-color: #06A5CF;
+    color: white;
+  }
+
+  ol {
+    padding-left: 0;
+  }
+
+  li {
+    background-color: #06A5CF;
+    border: 1px solid #06A5CF;
+    color: white;
+    font-weight: normal;
+    margin: 20px 0;
+    padding: 10px 20px;
+    text-align: left;
+    list-style-type: none;
+  }
+
+  li:hover {
+    background-color: white;
+    color: #06A5CF;
+    font-weight: bold;
+  }
+
+  .main-category-text,
+  .second-child-text {
+    font-weight: bold;
+  }
+
 </style>
